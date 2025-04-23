58566a37eaf9f6a2cecee2e801f1234356b243d3
Mincho1 <dinkovmincho@gmail.com>
2025-04-23 11:53:35 +0300

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/notenest-backoffice/',
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
+
 
